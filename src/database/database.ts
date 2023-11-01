c788a7ed5440303f5738ff374225c5bb832f238c
Wayne_Zhou <Wayne_Zhou@compal.com>
2023-11-01 10:28:18 +0800

    update ftp upload rule

--- src/database/database.ts
+++ src/database/database.ts
@@ -2,6 +2,7 @@ import * as mysql from 'mysql2/promise'
 import * as secret from '../../secret.json'
 import { Tables } from './infomation_schema'
 import dedent from 'dedent'
+import { date_to_format1 } from '../utils'
 
 
 class Database {
@@ -13,27 +14,51 @@ class Database {
 		this.conn?.destroy()
 	}
 	async select_items<TTable extends keyof Tables, TItems = Tables[TTable][]>(
-		table: TTable
+		table: TTable,
 	) {
 		const [data, header]: any[] = await this.conn.query(
 			`select * from \`${table}\``
-			)
+		)
+		return Array.from(data) as TItems
+	}
+	async select_items_where_time_in_range<TTable extends keyof Tables, TItems = Tables[TTable] extends { time: string } ? Tables[TTable][] : never>(
+		table: TTable,
+		from_time?: Date, to_time?: Date
+	) {
+		const today = ((date)=>{
+			date.setHours(0,0,0,0)
+			return date
+		})(new Date())
+		const tomorrow = ((date)=>{
+			date.setHours(0,0,0,0)
+			date.setDate(date.getDate()+1)
+			return date
+		})(new Date())
+		from_time = from_time || today
+		to_time = to_time || tomorrow
+		const from_time_str = date_to_format1(from_time || new Date())
+		const to_time_str = date_to_format1(to_time || new Date())
+		const [data, header]: any[] = await this.conn.query(
+			dedent`
+			select * from \`${table}\`
+			where \`time\` >= ? and \`time\` < ?
+			`, [from_time_str, to_time_str] 
+		)
 		return Array.from(data) as TItems
 	}
 	async insert_items<
-	TTable extends keyof Tables,
-	TItem = Tables[TTable],
-	TCols extends (keyof TItem)[] = []
+		TTable extends keyof Tables,
+		TItem = Tables[TTable],
+		TCols extends (keyof TItem)[] = []
 	>(
 		table: TTable,
 		cols: TCols,
 		data: Record<(typeof cols)[number], string>[]
 	) {
-		const cols_str = dedent`(\`${
-			cols.join("`, `")
-		}\`)`
-		const values_str = data.map(x=>`(${cols.map(x=>'?').join(', ')})`).join(', ')
-		const values = data.flatMap(x=> [...cols.map(c=>x[c])])
+		const cols_str = dedent`(\`${cols.join("`, `")
+			}\`)`
+		const values_str = data.map(x => `(${cols.map(x => '?').join(', ')})`).join(', ')
+		const values = data.flatMap(x => [...cols.map(c => x[c])])
 		const cmd = dedent`insert into \`${table}\`
 		${cols_str}
 		VALUES ${values_str};
--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,16 @@ import bodyParser from 'body-parser'
 import { database } from './database/database'
 import { UploadLogManager } from './upload_log'
 import { date_to_format1 } from './utils'
+import { setInterval } from 'timers'
 
 const ERROR_POST_DATA_INVALID = "post data invalid"
 
 async function main() {
 	const startup = express()
-	const manager = new UploadLogManager()
+	// const manager = new UploadLogManager()
 	startup.use(bodyParser.urlencoded({ extended: true }))
 	startup.use(bodyParser.json())
-	startup.use((req, res, next)=>{
+	startup.use((req, res, next) => {
 		console.log([
 			new Date().toLocaleString(),
 			req.method, req.path, req.body
@@ -26,6 +27,15 @@ async function main() {
 
 	startup.get("/api/punch_logs", async (req, res, handle_err) => {
 		try {
+			const data = await database.select_items_where_time_in_range('punch_log')
+			res.send(JSON.stringify(data))
+		} catch (error) {
+			handle_err(error)
+		}
+	})
+
+	startup.get("/api/punch_logs/all", async (req, res, handle_err) => {
+		try {
 			const data = await database.select_items('punch_log')
 			res.send(JSON.stringify(data))
 		} catch (error) {
@@ -35,32 +45,32 @@ async function main() {
 
 	startup.post("/api/punch_logs/add", async (req, res, handle_err) => {
 		try {
-			function resolve_single_added_data(post_data: any){
+			function resolve_single_added_data(post_data: any) {
 				const { time, user, device } = post_data
-				if(!time && ! user && ! device) {
+				if (!time && !user && !device) {
 					throw ERROR_POST_DATA_INVALID
 				}
 				const date = time ? new Date(time) : new Date()
-				const date_str = date_to_format1(date)	
+				const date_str = date_to_format1(date)
 				return {
 					time: date_str,
 					user_id: String(user),
 					device_id: String(device)
 				}
 			}
-			
+
 			const added_data: ReturnType<typeof resolve_single_added_data>[] = []
-			if(Array.isArray(req.body)){
-				added_data.push(...Array.from(req.body).map((x)=>{
+			if (Array.isArray(req.body)) {
+				added_data.push(...Array.from(req.body).map((x) => {
 					return resolve_single_added_data(x)
 				}))
-			}else{
+			} else {
 				added_data.push(resolve_single_added_data(req.body))
 			}
 
 			await database.insert_items('punch_log',
 				['time', 'user_id', 'device_id'], added_data)
-			await manager.upload(added_data)
+			// await manager.upload(added_data)
 			res.send(JSON.stringify({ message: "success" }))
 		} catch (error) {
 			handle_err(error)
@@ -69,19 +79,19 @@ async function main() {
 
 	startup.post("/api/punch_logs/upload", async (req, res, handle_err) => {
 		const data = await database.select_items('punch_log')
-		manager.upload_overwrite(data)
+		// manager.upload_overwrite(data)
 		res.send(JSON.stringify({ message: "success" }))
 	})
 
 	startup.use(((err, req, res, next) => {
 		if (req.path.startsWith('/api')) {
-			if(err == ERROR_POST_DATA_INVALID){
+			if (err == ERROR_POST_DATA_INVALID) {
 				res.status(400)
 				res.send({
 					message: "Bad Request",
 					err
 				})
-			}else {
+			} else {
 				res.status(500)
 				res.send({
 					message: "Internal Server Error",
@@ -95,10 +105,18 @@ async function main() {
 
 	console.log("Connecting to database")
 	await database.init()
-	await manager.init()
+	// await manager.init()
 	const server = http.createServer(startup)
 	server.listen(8080, "0.0.0.0")
 	console.log("Server listening at http://localhost:8080")
+	const interval_upload_to_ftp = setInterval(async () => {
+		const data = await database.select_items_where_time_in_range('punch_log')
+		const manager = new UploadLogManager()
+		await manager.init()
+		await manager.upload_overwrite(data);
+		manager.close()
+	}, 15 * 60 * 1000);
+
 	function handle(signal: number) {
 		console.log()
 		console.log("Got signal, closing server")
@@ -107,11 +125,11 @@ async function main() {
 		server.close()
 		console.log("Closing database")
 		database.close()
-		manager.close()
+		clearInterval(interval_upload_to_ftp);
+		// manager.close()
 	}
 	process.on("SIGINT", handle)
 	process.on("SIGTERM", handle)
-
 }
 
 
